f8f31e5136e6c06f6ba1c7fadf2902aa56c1a894
Murat <bayraktar.freelance@gmail.com>
2021-05-22 23:10:22 +0300

    update

--- Lab_Preliminary_Q2/lab2.c
+++ Lab_Preliminary_Q2/lab2.c
@@ -2,11 +2,15 @@
 #include "lab2_copy.h"
 
 /* TASK SOLUTIONS */
-
+/* done */
 void to_lower(char * sentence){
+    int i=0; char * c = (sentence + i);
+    do {
+        if ((*c >= 'A') && (*c <= 'Z')) *c += 32;
+    } while (*(c = (sentence + i++)) != '!' );
     return ;
 }
-
+/* done */
 void print_sentence(char * sentence){
     int i=0; char c = *(sentence + i++);
     do {
@@ -15,12 +19,31 @@ void print_sentence(char * sentence){
     printf("\n");
     return ;
 }
-
+/* done */
 void substrings(char * sentence, char * substr, int * arr){
+    int i=0, index = 0; char * c; 
+    while (*(c = (sentence + i++)) != '!')
+    {
+        if (*c == substr[0])
+        {
+            int k = 0;
+            int check = 1;
+            while (substr[k] != '!') 
+            {
+                if (substr[k] != *(c+k)) {check = 0; break;}
+                k++;
+            }
+            if (check) {
+                arr[index++] = i - 1;
+            }
+        }
+    }
+
     return ;
 }
 
 void removeX(char * sentence, int * arr, int count){
+
     return ;
 }
 
--- Lab_Preliminary_Q2/run.c
+++ Lab_Preliminary_Q2/run.c
@@ -3,8 +3,26 @@
 
 int main()
 {
-    char sentence[256] = "Preliminary Question for the Second Laboratory Exam!";
+    char sentence[256] = "This is the lab exam of CENG140 of Ceng department (in english version)!";
+
+    char substr[10] = "eng!";
+
+    int arr[10], i;
+
+    to_lower(sentence);
+
+    substrings(sentence, substr, arr);
+
+    /* Content of arr is {25,36,55} */
+
+    for (i = 0; i < 10; i++) {
+        printf("%d\n",arr[i]);
+    }
+
+    removeX(sentence, arr, 3);
 
     print_sentence(sentence);
+
+
     return 0;
 }
